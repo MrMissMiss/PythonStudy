0d880b880feb46a71ad34976ae8a479f23e310ba
MrMissMiss <106214777+MrMissMiss@users.noreply.github.com>
2022-05-27 17:43:01 +0800

    每周查账自动填写测试

--- pythonProject4practice/5.23-5.29 5楼/weekCheck.py
+++ pythonProject4practice/5.23-5.29 5楼/weekCheck.py
@@ -13,18 +13,37 @@ if __name__ == '__main__':
     # 找到父路径下的其他文件，即同级的其他文件
     file = (os.path.join(proDir, "租赁销售汇总导出.csv"))
 
+    shop_list = []
+    for root, dirs, files in os.walk(proDir):
+        for f in files:
+            if os.path.splitext(f)[1] == '.jpg':
+                shop_list.append(f.split('.')[0])
+    print(shop_list)
+
+
+
     data = pd.read_csv(file, encoding='gbk')
     shop_id = data["铺位号"].str.rstrip()
     shop_name = data["品牌"].str.rstrip()
     shop_amount = data["小计销售"]
 
+    context = []
     num = data.shape[0]
     for i in range(num):
-        context = {
-            "铺位号":shop_id[i],
-            "品牌":shop_name[i],
-            "小计销售":shop_amount[i]
-        }
-        print(context)
+        for n in shop_list:
+            if n in shop_name[i]:
+                context.append({
+                    "铺位号":shop_id[i],
+                    "品牌":shop_name[i].split('-')[1],
+                    "小计销售":shop_amount[i]
+                })
+                break
 
+    print(context)
+    print(len(context))
+
+    for shop_dict in context:
+        print(shop_dict['铺位号'])
+        print(shop_dict['品牌'])
+        print(shop_dict['小计销售'])
 
